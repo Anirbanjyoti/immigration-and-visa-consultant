3bb3a6df318b111d51186f9c83f7ed7eb1587245
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-17 17:38:29 +0530

    Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        partstheme: {
+          primary: "#162E40",
+          secondary: "#F6214B",
+          accent: "#0C2136",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
